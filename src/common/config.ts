5d863e98365d7be94e61199800fee36e4428031b
ychleo102615 <leo102615@gmail.com>
2023-01-24 16:47:30 +0800

    feat: 新增畫布

---
+++ src/common/config.ts
@@ -0,0 +1,5 @@
+const WIDTH = 16;
+const HEIGHT = 9;
+const RATIO = WIDTH / HEIGHT;
+
+export { WIDTH, HEIGHT, RATIO };
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,1 +1,29 @@
-<template>hello</template>
+<template>
+  <div
+    ref="screenRef"
+    class="bg-indigo-400 w-screen h-screen flex items-center justify-center"
+  >
+    <div
+      ref="canvasRef"
+      :class="canvasDependency"
+      class="bg-amber-200"
+      :style="{
+        aspectRatio: RATIO,
+      }"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { useElementSize } from "@vueuse/core";
+import { RATIO } from "@/common/config";
+
+const screenRef = ref();
+const canvasRef = ref();
+const { width, height } = useElementSize(screenRef);
+
+const canvasDependency = computed(() =>
+  width.value / height.value < RATIO ? "w-full" : "h-full"
+);
+</script>
